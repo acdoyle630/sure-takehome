c5b5a831d5f7334150b632881279128e5330cd63
adam <katclave@Kats-MBP.attlocal.net>
2022-12-07 09:01:39 -0800

    challenge-branch-map updated table to use map

--- fe-interview-technical-challenge-role-frontend-engineer/src/components/PolicyHolders/PolicyHoldersTable.tsx
+++ fe-interview-technical-challenge-role-frontend-engineer/src/components/PolicyHolders/PolicyHoldersTable.tsx
@@ -6,9 +6,6 @@ import { usePolicyHolderState, Address } from './PolicyHoldersProvider';
 const PolicyHoldersTable = () => {
   const { policyHolders } = usePolicyHolderState();
 
-  const firstPolicy = policyHolders[0];
-  const secondaryPolicy = policyHolders[1];
-
   const formatAddress = ({
     line1,
     line2,
@@ -20,40 +17,28 @@ const PolicyHoldersTable = () => {
 ${city}, ${state} ${postalCode}`;
   };
 
-  const rows =
-    policyHolders.length > 0
-      ? [
-          { key: 'Name', value: firstPolicy.name },
-          { key: 'Age', value: firstPolicy.age },
-          { key: 'Address', value: formatAddress(firstPolicy.address) },
-          { key: 'Phone Number', value: firstPolicy.phoneNumber },
-          {
-            key: 'Primary policyholder',
-            value: firstPolicy.isPrimary ? 'True' : 'False',
-          },
-        ]
-      : [];
-
-  const secondaryRow =
-    policyHolders.length > 1
-      ? [
-          { key: 'Name', value: secondaryPolicy.name },
-          { key: 'Age', value: secondaryPolicy.age },
-          { key: 'Address', value: formatAddress(secondaryPolicy.address) },
-          { key: 'Phone Number', value: secondaryPolicy.phoneNumber },
-          {
-            key: 'Primary policyholder',
-            value: secondaryPolicy.isPrimary ? 'True' : 'False',
-          },
-        ]
-      : [];
-
   return (
     <Box>
-      <InfoTable sx={{ mb: 2 }} header="Primary Policy Holder" rows={rows} />
-      {policyHolders.length > 1 ? (
-        <InfoTable header="Additional Policy Holder" rows={secondaryRow} />
-      ) : null}
+      {policyHolders.map((holder) => (
+        <InfoTable
+          sx={{ mb: '10px' }}
+          header={
+            holder.isPrimary
+              ? 'Primary Policy Holder'
+              : 'Additional Policy Holder'
+          }
+          rows={[
+            { key: 'Name', value: holder.name },
+            { key: 'Age', value: holder.age },
+            { key: 'Address', value: formatAddress(holder.address) },
+            { key: 'Phone Number', value: holder.phoneNumber },
+            {
+              key: 'Primary policyholder',
+              value: holder.isPrimary ? 'True' : 'False',
+            },
+          ]}
+        />
+      ))}
     </Box>
   );
 };
